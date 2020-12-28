057ca335af7749e37951ba40a215ef592f1d6051
Bemnet Dejene <bdejene19@gmail.com>
2020-12-27 19:38:49 -0500

    Updates

---
+++ precache-manifest.9dd32b6b50368bef3cd535c6cff4ba79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9a7b52814ba2f68a92d3cdc6bb7d6c",
+    "url": "/ArsenalSoccerFansPage/index.html"
+  },
+  {
+    "revision": "4c92c917a31320bc95ef",
+    "url": "/ArsenalSoccerFansPage/static/css/main.65f5824e.chunk.css"
+  },
+  {
+    "revision": "795cddffb497412dd500",
+    "url": "/ArsenalSoccerFansPage/static/js/2.da574b90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ArsenalSoccerFansPage/static/js/2.da574b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c92c917a31320bc95ef",
+    "url": "/ArsenalSoccerFansPage/static/js/main.6d07d8cb.chunk.js"
+  },
+  {
+    "revision": "e263379ba3cab8411b36",
+    "url": "/ArsenalSoccerFansPage/static/js/runtime-main.f4530291.js"
+  }
+]);
